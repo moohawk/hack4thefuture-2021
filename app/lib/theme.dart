451ee6b15857e566013149fcb8cf788b553c5933
BB <boyan@typed.wtf>
2021-02-27 14:12:27 +0200

    Add the theme.

---
+++ app/lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static const Color primary = Color(0xFF6FCDE0);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF253840);
+
+  static const String fontName = 'Roboto';
+}
