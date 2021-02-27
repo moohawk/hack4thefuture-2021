e5091e13803d81b8b447bdec48567d7d386a67f3
Stefan Stamenov <stefan.stamenov1996@gmail.com>
2021-02-27 12:34:05 +0200

    It's not working just switching the work places

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from typing import Optional
 import sqlalchemy
 
@@ -30,14 +32,43 @@ class Coordinates(BaseModel):
     latitude: float
     longitude: float
 
-class SearchRequest(BaseModel):
+class RestaurantSearchRequest(BaseModel):
     s: Optional[str] = None
     ne: Optional[Coordinates] = None
     sw: Optional[Coordinates] = None
 
 @app.post("/restaurants/search")
-async def restaurants_search(sr: SearchRequest):
+async def restaurants_search(sr: RestaurantSearchRequest):
     sql = text("""SELECT id, name,  ST_Y(coordinates) as lat, ST_X(coordinates) as lon, address FROM public.restaurants
+    WHERE ST_Contains(st_makeenvelope(:min_x, :min_y, :max_x, :max_y, 4326), coordinates) AND name != '' LIMIT 5""")
+    rs = engine.execute(sql, {"min_x": sr.sw.longitude, "min_y": sr.sw.latitude, "max_x": sr.ne.longitude, "max_y": sr.ne.latitude})
+    search_results = []
+    for r in rs:
+        search_results.append(dict(r))
+    return search_results
+
+
+class ContributionSearchRequest(BaseModel):
+    s: Optional[str] = None
+    coordinates: Optional[Coordinates] = None
+
+class ValidTime(BaseModel):
+    valid_from: datetime
+    valid_to: datetime
+
+class Contribution(BaseModel):
+    title: str
+    retstaurant: Restaurant
+    valid_time: ValidTime
+    status: string
+
+@app.get("/contributions/search")
+async def contributions_search(sr: ContributionSearchRequest):
+    sql = text("""SELECT id as contribution_id, title, lower(valid_time) as valid_from, upper(valid_time) as valid_to, status, 
+                name,  ST_Y(coordinates) as lat, ST_X(coordinates) as lon, address FROM public.contributions 
+                LEFT JOIN public.restaurants on restaurants.id = contributions.restaurant_id """)
+
+    sql = text("""SELECT id, title,  ST_Y(coordinates) as lat, ST_X(coordinates) as lon, address FROM public.restaurants
     WHERE ST_Contains(st_makeenvelope(:min_x, :min_y, :max_x, :max_y, 4326), coordinates) AND name != '' LIMIT 5""")
     rs = engine.execute(sql, {"min_x": sr.sw.longitude, "min_y": sr.sw.latitude, "max_x": sr.ne.longitude, "max_y": sr.ne.latitude})
     search_results = []
