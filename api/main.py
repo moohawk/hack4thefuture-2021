bd683e6bf4cb024dbfed3e9d9010c666aa5b2aa3
BB <boyan@typed.wtf>
2021-02-27 10:25:52 +0200

    Try to use request shapes.

--- api/main.py
+++ api/main.py
@@ -11,7 +11,6 @@ engine = sqlalchemy.create_engine(
     'postgres://root:root@localhost:5432/food_for_thought'
 )
 
-
 class Restaurant(BaseModel):
     name: str
     address: str
@@ -26,13 +25,18 @@ async def read_root():
 async def get_all_restaurants():
     return [{"Restaurant":1},{"Restaurant":2}]
 
-@app.get("/restaurants/search")
-async def get_restaurants_by_location(latitude: float = 0.0, longitude: float = 0.0):
-    sql = text('select name from restaurants')
-    result = engine.execute(sql)
-    names = [row[0] for row in result]
-    print(names)
 
+class Coordinates(BaseModel):
+    latitude: float
+    longitude: float
+
+class SearchRequest(BaseModel):
+    s: str
+    ne: Coordinates
+    sw: Coordinates
+
+@app.post("/restaurants/search")
+async def restaurants_search(sr: SearchRequest):
     return [
         {
             "label": "Chevermeto",
