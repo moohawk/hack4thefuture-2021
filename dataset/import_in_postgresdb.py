78489b0bd0a91470a4f048c636d8508dfddbf84e
BB <boyan@typed.wtf>
2021-02-27 09:47:10 +0200

    Call DB food_for_thought, remove non-binary dep (requires local psql)

--- dataset/import_in_postgresdb.py
+++ dataset/import_in_postgresdb.py
@@ -13,7 +13,7 @@ with open('20210226221543076_unique.csv') as csv_file:
 
 
 # Connect to DB
-conn = psycopg2.connect(database="restaurants", user='root', password='root', host='127.0.0.1', port='5432')
+conn = psycopg2.connect(database="food_for_thought", user='root', password='root', host='127.0.0.1', port='5432')
 conn.autocommit = True
 
 cursor = conn.cursor()
@@ -22,11 +22,11 @@ for item in restaurants[1:]:
     # Skip if latitude or longtitude is missing
     if not item[2] or not item[3]:
         continue
-    # Replace because of the apostrophes 
+    # Replace because of the apostrophes
     item[0] = item[0].replace("'","''")
-    cursor.execute(f'''INSERT INTO restaurants(id, address, coordinates, name) 
-                      VALUES (uuid_generate_v4(), '{item[1]}', 
-                      ST_SetSRID(ST_MakePoint(cast({item[2]} as double precision), 
+    cursor.execute(f'''INSERT INTO restaurants(id, address, coordinates, name)
+                      VALUES (uuid_generate_v4(), '{item[1]}',
+                      ST_SetSRID(ST_MakePoint(cast({item[2]} as double precision),
                                             cast({item[3]} as double precision))::geography,4326)::geometry, E'{item[0]}')''')
 
 conn.commit()
