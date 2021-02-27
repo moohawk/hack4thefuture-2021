f50c0f628e0c708a37a77f8db00dfc9e2f094613
BB <boyan@typed.wtf>
2021-02-27 10:03:04 +0200

    Add sqlalchemy and connect it .

--- api/main.py
+++ api/main.py
@@ -1,16 +1,22 @@
 from typing import Optional
+import sqlalchemy
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlalchemy import text
 
 app = FastAPI()
+
+engine = sqlalchemy.create_engine(
+    'postgres://root:root@localhost:5432/food_for_thought'
+)
+
 
 class Restaurant(BaseModel):
     name: str
     address: str
     latitude: float
     longitude: float
-
 
 @app.get("/")
 async def read_root():
@@ -22,6 +28,11 @@ async def get_all_restaurants():
 
 @app.get("/restaurants/search")
 async def get_restaurants_by_location(latitude: float = 0.0, longitude: float = 0.0):
+    sql = text('select name from restaurants')
+    result = engine.execute(sql)
+    names = [row[0] for row in result]
+    print(names)
+
     return [
         {
             "label": "Chevermeto",
